807d7448098de2465e9913204b64e266215780b0
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 12:05:36 +0100

    Add files via upload

---
+++ exercice7/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 3</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 7</h1>
+    <?php
+    
+    for ($count = 1; $count <= 100; $count = $count+15)
+    {
+        echo 'On tient le bon bout.<br />';
+    }
+    
+    ?>  
+    
+    
+</body>
+</html>
