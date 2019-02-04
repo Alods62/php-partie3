5c799c7489daa020ac196e90dfb9800909d9f043
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 12:00:58 +0100

    Add files via upload

---
+++ exercice1/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 3</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 1</h1>
+    <?php
+    
+    $count = 0;
+    
+    while ($count < 10) // while ou tant que ... ici tant que count est inférieur à 10
+    {
+        echo $count;
+        $count++; // incrémentation
+    }
+    
+    ?>  
+    
+    
+</body>
+</html>
