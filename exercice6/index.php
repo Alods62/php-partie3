85948b13adf3858c8e4605b295215a8dde741d27
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 12:04:50 +0100

    Add files via upload

---
+++ exercice6/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 3</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 6</h1>
+    <?php
+    
+    for ($count = 20; $count >= 0; $count--)
+    {
+        echo 'C\'est presque bon.<br />';
+    }
+    ?>  
+    
+    
+</body>
+</html>
