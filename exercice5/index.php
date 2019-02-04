16aef6bb02bd92880feb03c95bd0b3311b0bc605
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 12:04:10 +0100

    Add files via upload

---
+++ exercice5/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 3</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 5</h1>
+    <?php
+    
+    for ($count = 1; $count <= 15; $count++) // boucle for (on initialise une variable qui va servir de compteur; on choisit une condition pour rester dans la boucle; on incrémente le compteur)
+    {
+        echo 'On y arrive presque.<br />';
+    }
+    
+    ?>  
+    
+    
+</body>
+</html>
