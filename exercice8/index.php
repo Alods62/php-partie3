fd6efa0bfeb31c6954d4a55f14d8995f1997350d
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 12:12:27 +0100

    Add files via upload

---
+++ exercice8/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 3</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 8</h1>
+    <?php
+    
+    for ($count = 200; $count >= 0; $count -= 12)
+    {
+        echo 'Enfin !!!!<br />';
+    }
+    
+    ?>  
+    
+    
+</body>
+</html>
